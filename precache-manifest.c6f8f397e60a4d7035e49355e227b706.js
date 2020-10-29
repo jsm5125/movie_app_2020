68e758d4c962d57cd929359e4cb1a70b9878adb8
smJung <jsm5125@naver.com>
2020-10-30 02:12:16 +0900

    Updates

---
+++ precache-manifest.c6f8f397e60a4d7035e49355e227b706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe2602b1cd1a44a527b857195ab470f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "16cf4e7bb7c12d2ec5c8",
+    "url": "/movie_app_2020/static/css/main.63cb3386.chunk.css"
+  },
+  {
+    "revision": "875c359fdca8c40ef3a6",
+    "url": "/movie_app_2020/static/js/2.b339c686.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/movie_app_2020/static/js/2.b339c686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cf4e7bb7c12d2ec5c8",
+    "url": "/movie_app_2020/static/js/main.0bf63485.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
